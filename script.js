afd67532f0c664098c24e9f4dd146869c9881413
glezzz <alexglezk@gmail.com>
2020-09-16 15:37:44 +0200

    getting temps starts working

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ function getWeather() {
         .then(response => response.json())
         .then(weather => {
             console.log(weather);
+
             let nextFive = weather.list.slice(0, 5);     // .slice() method to get a portion of the array
             console.log(nextFive);
+
+            let minFahr
+            let maxFahr
+            let avgFahr
+
+            nextFive.forEach(day =>{
+                minFahr = (day.main.temp_min);
+                console.log(minFahr);
+
+            })
+
+
         });
 
-    
+
 }
 
 
