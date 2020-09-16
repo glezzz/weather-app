e28504cd4d6612dff03902f1d5e4799e19b2c3e5
glezzz <alexglezk@gmail.com>
2020-09-16 16:48:27 +0200

    have metric temps

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 const key = config.MY_KEY;
+let url = "http://api.openweathermap.org/data/2.5/forecast?q=";
 
 function getWeather() {
     let city = document.getElementById("city").value;
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key)
+    fetch(url + city + "&units=metric&appid=" + key)
         .then(response => response.json())
         .then(weather => {
             console.log(weather);
@@ -11,18 +12,7 @@ function getWeather() {
             let nextFive = weather.list.slice(0, 5);     // .slice() method to get a portion of the array
             console.log(nextFive);
 
-            let minFahr
-            let maxFahr
-            let avgFahr
-
-            nextFive.forEach(day =>{
-                minFahr = (day.main.temp_min);
-                console.log(minFahr);
-
-            })
-
-
-        });
+        })
 
 
 }
