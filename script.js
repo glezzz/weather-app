9f7079738f088c5066bee1f90d80a2aa9bd02ad9
glezzz <alexglezk@gmail.com>
2020-09-18 01:17:29 +0200

    display city temps icons

--- script.js
+++ script.js
@@ -11,15 +11,16 @@ function getWeather() {
             let name = data.city.name;
             let lon = data.city.coord.lon;
             let lat = data.city.coord.lat;
-            console.log(name + " " + lon + " " + lat);
+            document.getElementById("name").innerHTML = data.city.name + ", " + data.city.country;
             getTemps(lat,lon);
 
 
         })
 
 }
+const oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="
 
-function getTemps(lat, lon){ fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" +key)
+function getTemps(lat, lon){ fetch(oneCallUrl + lat + "&lon=" + lon + "&units=metric&appid=" + key)
     .then(response => response.json())
     .then(data => {
         console.log(data)
@@ -30,27 +31,52 @@ function getTemps(lat, lon){ fetch("https://api.openweathermap.org/data/2.5/onec
         //console.log(descOne);
         let dayTwo = data.daily[1].temp.day;
         let descTwo = data.daily[1].weather[0].description;
+        let iconTwo = data.daily[1].weather[0].icon;
         //console.log(descTwo);
         let dayThree= data.daily[2].temp.day;
         let descThree = data.daily[2].weather[0].description;
+        let iconThree = data.daily[2].weather[0].icon;
         //console.log(dayThree);
         let dayFour = data.daily[3].temp.day;
         let descFour = data.daily[3].weather[0].description;
+        let iconFour = data.daily[3].weather[0].icon;
         //console.log(dayFour);
         let dayFive = data.daily[4].temp.day;
         let descFive = data.daily[4].weather[0].description;
+        let iconFive = data.daily[4].weather[0].icon;
         //console.log(dayFive);
 
-        display(dayOne, descOne, dayTwo, descTwo, dayThree, descThree, dayFour, descFour);
+        display(dayOne, descOne, iconOne, dayTwo, descTwo, iconTwo,
+                dayThree, descThree, iconThree, dayFour, descFour, iconFour, dayFive, descFive, iconFive);
 
     })
 
 }
 
-function display(dayOne, descOne, dayTwo, descTwo, dayThree, descThree, dayFour, descFour){
-    document.getElementById("temp").innerHTML = dayOne;
-    document.getElementById("desc").innerHTML = descOne;
-    document.getElementById("desc").innerHTML =
+const iconUrl = "http://openweathermap.org/img/wn/"
+
+function display(dayOne, descOne, iconOne, dayTwo, descTwo, iconTwo, dayThree, descThree, iconThree,
+                 dayFour, descFour, iconFour, dayFive, descFive, iconFive ){
+    document.getElementById("temp1").innerHTML = Math.round(dayOne) + "°C";
+    console.log(dayOne);
+    document.getElementById("desc1").innerHTML = descOne;
+    document.getElementById("icon1").src = iconUrl+ iconOne + "@2x.png";
+
+    document.getElementById("temp2").innerHTML = Math.round(dayTwo) + "°C";
+    document.getElementById("desc2").innerHTML = descTwo;
+    document.getElementById("icon2").src = iconUrl + iconTwo + "@2x.png";
+
+    document.getElementById("temp3").innerHTML = Math.round(dayThree) + "°C";
+    document.getElementById("desc3").innerHTML = descThree;
+    document.getElementById("icon3").src = iconUrl + iconThree + "@2x.png";
+
+    document.getElementById("temp4").innerHTML = Math.round(dayFour) + "°C";
+    document.getElementById("desc4").innerHTML = descFour;
+    document.getElementById("icon4").src = iconUrl + iconFour + "@2x.png";
+
+    document.getElementById("temp5").innerHTML = Math.round(dayFive) + "°C";
+    document.getElementById("desc5").innerHTML = descFive;
+    document.getElementById("icon5").src = iconUrl + iconFive + "@2x.png";
 }
 
 
